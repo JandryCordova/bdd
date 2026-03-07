bea9cc7bf8d0d8b52dac99edf60c38678e6a3cc0
JandryCordova <jandry.torres.03@gmail.com>
2026-03-06 22:56:02 -0500

    Ingresamos en total 8 videojuegos

--- inserciones/TablaVideojuegos.sql
+++ inserciones/TablaVideojuegos.sql
@@ -5,3 +5,29 @@ create table videojuegos (
 	valoracion int not null,
 	constraint videojuegos_pk primary key (codigo)
 )
+--Insertar 5 videojuegos con todas la columnas
+insert into videojuegos (codigo, nombre, descripcion, valoracion)
+values (11111,'Resident Evil 4','Juego Sangriento',10)
+
+insert into videojuegos (codigo, nombre, descripcion, valoracion)
+values (22222,'GTA San Andreas','Juego de Simulacion',10)
+
+insert into videojuegos (codigo, nombre, descripcion, valoracion)
+values (33333,'Free Fire','Juego de Comabate',9)
+
+insert into videojuegos (codigo, nombre, descripcion, valoracion)
+values (44444,'Point Black','Juego Guerra',10)
+
+insert into videojuegos (codigo, nombre, descripcion, valoracion)
+values (55555,'Call of Duty','Juego Sangriento',8)
+--Insertar 3 Videojuegos solo con los campos obligatorios
+insert into videojuegos (codigo, nombre, valoracion)
+values (66666,'Dragon City',10)
+
+insert into videojuegos (codigo, nombre, valoracion)
+values (77777,'City Ville',10)
+
+insert into videojuegos (codigo, nombre, valoracion)
+values (88888,'Empire & allies',10)
+--Verificamos la tabla
+select * from videojuegos
