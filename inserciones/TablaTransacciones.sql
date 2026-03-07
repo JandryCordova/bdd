752f451836cfb288dc487774b8169f4c060fe145
JandryCordova <jandry.torres.03@gmail.com>
2026-03-06 23:08:35 -0500

    Insertamos 10 transacciones

--- inserciones/TablaTransacciones.sql
+++ inserciones/TablaTransacciones.sql
@@ -7,3 +7,35 @@ create table transacciones (
 	hora time not null,
 	constraint transacciones_pk primary key (codigo)
 )
+--Inserta 10 transacciones
+insert into transacciones (codigo, numero_cuenta, monto, tipo , fecha, hora)
+values (11111,'99999','26.54','C','10/02/2026','20:13')
+
+insert into transacciones (codigo, numero_cuenta, monto, tipo , fecha, hora)
+values (22222,'88888','56,89','D','10/03/2026','20:14')
+
+insert into transacciones (codigo, numero_cuenta, monto, tipo , fecha, hora)
+values (33333,'77777','85,65','C','10/04/2026','20:15')
+
+insert into transacciones (codigo, numero_cuenta, monto, tipo , fecha, hora)
+values (44444,'66666','102,58','D','10/05/2026','20:16')
+
+insert into transacciones (codigo, numero_cuenta, monto, tipo , fecha, hora)
+values (55555,'55555','855,65','C','10/06/2026','20:17')
+
+insert into transacciones (codigo, numero_cuenta, monto, tipo , fecha, hora)
+values (66666,'44444','32,65','D','10/07/2026','20:18')
+
+insert into transacciones (codigo, numero_cuenta, monto, tipo , fecha, hora)
+values (77777,'33333','95,69','C','10/08/2026','20:19')
+
+insert into transacciones (codigo, numero_cuenta, monto, tipo , fecha, hora)
+values (88888,'22222','365,55','D','10/09/2026','20:20')
+
+insert into transacciones (codigo, numero_cuenta, monto, tipo , fecha, hora)
+values (99999,'11111','845,52','C','10/10/2026','20:21')
+
+insert into transacciones (codigo, numero_cuenta, monto, tipo , fecha, hora)
+values (89756,'12345','635,25','D','10/11/2026','20:22')
+--Verificamos la Tabla
+select * from transacciones
