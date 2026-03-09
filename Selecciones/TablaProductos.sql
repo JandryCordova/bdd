ad51c9ec0f09c383efb6a3e7357f50609841052d
JandryCordova <jandry.torres.03@gmail.com>
2026-03-09 13:01:16 -0500

    Arreglo de la seleccion de la tabla productos

--- Selecciones/TablaProductos.sql
+++ Selecciones/TablaProductos.sql
@@ -38,4 +38,4 @@ select * from productos where nombre like 'Q%'
 
 select * from productos where descripcion is null
 
-select * from productos where precio between '2' and '3'
+select * from productos where precio between money(2) and money(3)
